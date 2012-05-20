aecb98785b41b4c3d6c53272381498a4a3f32cee
Natalia Zon <natalia.zon@gmail.com>
2012-05-20 12:04:11 +0200

    added user model + passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,13 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :about, :active, :birthdate, :country, :email, :name, :webpage
+  attr_accessible :about, :active, :birthdate, :country, :email, :name, :webpage, :password, :password_confirmation
+  has_secure_password
   before_save { |user| user.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :password_confirmation, presence: true
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120520080425) do
+ActiveRecord::Schema.define(:version => 20120520095022) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(:version => 20120520080425) do
     t.string   "webpage"
     t.string   "country"
     t.boolean  "active"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
